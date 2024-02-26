bf1c3e2bd8331e873662ecf15f9b565facbb0ff7
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2024-02-26 15:43:23 -0300

    Refactor code structure and remove unnecessary variables.

--- src/Index.ts
+++ src/Index.ts
@@ -16,9 +16,7 @@ player1.levelUp();
 
 const monster1 = new Monster();
 const monster2 = new Dragon();
-
 const pvp = new PVP(player2, player3);
-
 const pve = new PVE(player1, [monster1, monster2]);
 
 const runBattles = (battles: Battle[]): void => {
