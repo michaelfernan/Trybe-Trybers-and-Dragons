834bd285136ca172aef38ea6c3640e49c5b64385
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2024-02-26 15:51:48 -0300

    Renamed and removed some private variables in Archetype.

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,15 +1,10 @@
 import { EnergyType } from '../Energy';
 
 export default abstract class Archetype {
-  private _name: string;
-  private _special: number;
-  private _cost: number;
+  private _special = 0;
+  private _cost = 0;
 
-  constructor(name: string) {
-    this._name = name;
-    this._special = 0;
-    this._cost = 0;
-  }
+  constructor(private _name: string) {}
 
   get name(): string {
     return this._name;
